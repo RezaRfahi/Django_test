ac43862001e22c0999cd9a6bf73e226887bfbf60
RezaRfahi <refahireza82@gmail.com>
2021-07-26 12:47:53 +0430

    Create Models
    Create class and field in models

--- first_app/models.py
+++ first_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Author(models.Model):
+    name=models.CharField(max_length=20)
+    family=models.CharField(max_length=20)
+
+class Info(models.Model):
+    author=models.ForeignKey(Author,on_delete=models.CASCADE)
+    title=models.CharField(max_length=25)
+    dsc=models.TextField()
+    img=models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=None)
+    pub_date=models.DateTimeField()

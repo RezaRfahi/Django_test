939ef4d31616b7e0a50d24c262b098dbbf0e8c0b
RezaRfahi <refahireza82@gmail.com>
2021-08-13 21:24:21 +0430

    set url signin
    connect url to signin views

--- first_app/urls.py
+++ first_app/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
     path('',views.index.as_view(),name='index'),
     path('about/',views.about.as_view(),name='about'),
     path('<int:pk>/',views.info_detail.as_view(),name='detail'),
-    path('<int:pk>/',views.addComment,name='comment')   
+    path('<int:pk>/',views.addComment,name='comment'),
+    path('accounts/signin',views.signin.as_view(),name='signin')  
 ]
 urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
